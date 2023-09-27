267847e7e1921ff4f9fb7c40ec89549cd7ae4ec9
Alisson de Andrade Araujo <alisson.araujo@sefaz.pb.gov.br>
2023-09-27 07:11:41 -0300

    Icon

--- src/components/description/index.tsx
+++ src/components/description/index.tsx
@@ -59,7 +59,7 @@ export function Description() {
             
             </div>
 
-            <strong style={{ color:"#3294f8", marginTop: "-80px", marginLeft: "160px", fontSize: "20px" }} ><a href={`https://github.com/${login}`} target="_blank" rel="guthub"><ArrowUpRight  size={25} color="#3294f8" style={{ marginLeft: "80px"}}/><p style={{ marginTop: "-30px", color:"#3294f8", textDecoration: "none" }}>GITHUB</p> </a> 
+            <strong style={{ color:"#3294f8", marginTop: "-80px", marginLeft: "160px", fontSize: "20px" }} ><a href={`https://github.com/${login}`} target="_blank" rel="guthub"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#3294f8"  style={{ marginLeft: "80px"}} viewBox="0 0 256 256"><path d="M228,104a12,12,0,0,1-24,0V69l-59.51,59.51a12,12,0,0,1-17-17L187,52H152a12,12,0,0,1,0-24h64a12,12,0,0,1,12,12Zm-44,24a12,12,0,0,0-12,12v64H52V84h64a12,12,0,0,0,0-24H48A20,20,0,0,0,28,80V208a20,20,0,0,0,20,20H176a20,20,0,0,0,20-20V140A12,12,0,0,0,184,128Z"></path></svg><p style={{ marginTop: "-26.5px", color:"#3294f8", textDecoration: "none" }}>GITHUB</p> </a> 
             </strong>
              </> 
             )
